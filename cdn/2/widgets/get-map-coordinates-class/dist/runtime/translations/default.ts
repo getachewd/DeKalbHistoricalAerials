96471ec18b2fe89caff03d681cacc4e105566909
Getachew <getachewb2012@gmail.com>
2024-07-19 11:09:22 -0400

    Inial Commit

---
+++ cdn/2/widgets/get-map-coordinates-class/dist/runtime/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  _widgetLabel: "Google Maps",
+  latLon: "Lat/Lon",
+  zoom: "Zoom",
+  latLonWillBeHere: "Lat/Lon (None - please mouse over map)"
+};
---
+++ cdn/2/widgets/get-map-coordinates-class2/dist/runtime/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  _widgetLabel: "Pictometry",
+  latLon: "Lat/Lon",
+  zoom: "Zoom",
+  latLonWillBeHere: "Lat/Lon (None - please mouse over map)"
+};
